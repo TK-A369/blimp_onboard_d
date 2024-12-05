5660f476044cd2b9ecdd557aa35343fd0897f116
Tomasz Kawalec <tkawalec369@gmail.com>
2024-12-05 19:15:15 +0100

    Invoke start_communication function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
+use tokio;
+
+use blimp_comm;
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+    let channels = vec![blimp_comm::CommChannel::Server(core::net::SocketAddr::new(
+        std::net::IpAddr::V4(core::net::Ipv4Addr::new(127, 0, 0, 1)),
+        9898,
+    ))];
+    blimp_comm::start_communication(channels).await;
+
+    tokio::signal::ctrl_c().await.unwrap();
 }
